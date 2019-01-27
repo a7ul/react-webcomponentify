4b7d6a7beaf92ba1c6bff2ccdd3618a89a36d385
Atul <atulanand94@gmail.com>
2019-01-28 00:06:48 +0100

    Bug fix for child components

--- src/react-dom-child.js
+++ src/react-dom-child.js
@@ -8,8 +8,8 @@ import createRef from "react-create-ref";
 export class ReactDomChild extends React.Component {
   ref = createRef();
   componentDidMount() {
-    const children = this.props.children || [];
-    this.ref.current.replaceWith(...children);
+    const childNodes = this.props.children[0];
+    this.ref.current.replaceWith(...childNodes);
   }
   render() {
     return <div ref={this.ref} />;
